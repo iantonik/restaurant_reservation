53dd87419bd1fd92ca183fe4b4922995a17517b4
Shy <shyloh.elder@gmail.com>
2019-04-06 12:03:17 -0400

    changes 1

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+
 var express = require("express");
 var path = require("path");
 
@@ -7,3 +8,21 @@ var PORT = (process.env.PORT || 5000)
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+var reservations = [
+    
+];
+
+//routes
+
+app.get("/", function(req, res){
+    res.sendFile(path.join(__dirname, "index.html"))
+});
+
+app.get("/reservation", function(req, res){
+    res.sendFile(path.join(__dirname, "reservation.html"))
+});
+
+app.get("/viewtables", function(req, res){
+    res.sendFile(path.join(__dirname, "viewtables.html"))
+})
