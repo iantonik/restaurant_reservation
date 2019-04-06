5483fad40d84680c0304a6288647454ded14eca5
Shy <shyloh.elder@gmail.com>
2019-04-06 12:09:11 -0400

    adding reservation and viewtables html

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ var PORT = (process.env.PORT || 5000)
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+
 var reservations = [
     
 ];
